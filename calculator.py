f9772fba22c347f8d1b9f9eb07c9b930a76f71a8
ephatus <ephantuskagwi2@gmail.com>
2025-07-21 14:40:55 +0300

    calculator ya test

---
+++ calculator.py
@@ -0,0 +1,10 @@
+num1 = float(input("enter the first number"))
+num2 = float(input("enter the first number"))
+sum = num1 + num2
+product = num1 * num2
+quoetient = num1 / num2
+difference = num1 - num2
+print("The sum is", sum)
+print("The product is", product)    
+print("The quotient is", quoetient)
+print("The difference is", difference) 
