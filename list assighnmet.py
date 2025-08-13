c27327535cdbb971ca7f0672863186fbfe9e7414
ephatus <85250753+ephatus@users.noreply.github.com>
2025-08-13 16:30:16 +0300

    list work assighnmet

---
+++ list assighnmet.py
@@ -0,0 +1,17 @@
+my_list = []
+my_list.append(5)
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list )
+my_list[1] = 15
+print(my_list)
+mylist2 = [50, 60, 70]
+my_list.extend(mylist2)
+print(my_list)
+my_list.pop()
+print(my_list)
+my_list.sort()
+print(my_list)
+print(my_list[30])
